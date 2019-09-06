2037a2650a892d6e893fe3c4e16a6ea2d240a9cd
Nicolás Riquelme <nrriquelme@gmail.com>
2019-09-06 00:12:00 -0400

    WIP: user boxes

---
+++ src/views/shared/UserBox/UserUIBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UserUIBox = props => (
+  <div>
+    <h1>UserUIBox</h1>
+  </div>
+);
+
+export default UserUIBox;
---
+++ src/views/shared/UserBox/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UserUIBox from "./UserUIBox";
+
+class UserBox extends React.Component {
+  render() {
+    return <UserUIBox />;
+  }
+}
+
+export default UserBox;
