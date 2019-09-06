721ff5ee5eaa8471e632b1e56616fb835cd57ef6
Nicolás Riquelme <nrriquelme@gmail.com>
2019-09-06 01:14:31 -0400

    WIP: styles

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import UserBox from "../shared/UserBox";
 import UserInfoBox from "../shared/UserInfoBox";
 
+import "./Home.scss";
+
 class Home extends React.Component {
   async componentDidMount() {
     const { localStorageKey, UserService, requestSize } = this.props;
@@ -14,22 +16,22 @@ class Home extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <div className="container">
-        <div className="header">
+      <div className="user-container">
+        <div className="user-header">
           <h2>Random User Interface!</h2>
         </div>
         <div className="user-actions">
           <h3>Acciones</h3>
-          <div className="actions-container">
+          <div className="user-actions-container">
             <button className="table-action">Ver todos</button>
             <button className="remove-action">Eliminar actual</button>
           </div>
         </div>
-        <div className="user-container">
-          <div className="user-info">
+        <div className="user-container-info">
+          <div className="user-info info-box">
             <UserInfoBox />
           </div>
-          <div className="user-form">
+          <div className="user-info info-form">
             <UserBox />
           </div>
         </div>
