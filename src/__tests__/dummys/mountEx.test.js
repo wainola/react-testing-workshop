c934fb1dd08513e6a69911bfe529cb30c5946fa8
Nicolás Riquelme <nrriquelme@gmail.com>
2019-09-06 22:40:09 -0400

    Mount example

---
+++ src/__tests__/dummys/mountEx.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+
+const Button = ({ text, classStyle }) => (
+  <button className={classStyle}>{text}</button>
+);
+
+const Parent = ({ style, data }) =>
+  data.map((_, idx) => (
+    <Button
+      key={`someKey${idx}`}
+      classStyle={style}
+      text={`Button number ${idx + 1}`}
+    />
+  ));
+
+describe("<Paren />", () => {
+  let mt;
+  test("Deberia renderear 3 botones", () => {
+    const a = [1, 2, 3];
+    mt = mount(<Parent style="funky-class" data={a} />);
+    // console.log(mt.debug());
+    const btns = mt.find("Button");
+    expect(btns).toHaveLength(3);
+  });
+  test("Los tres botones deberian tener clase funky-class", () => {
+    const btns = mt.find("button");
+    btns.forEach(node => expect(node.hasClass("funky-class")).toBe(true));
+  });
+});
