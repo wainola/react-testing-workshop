99c373d04ada09abc748491a60544b7d3152d4d4
Nicolás Riquelme <nrriquelme@gmail.com>
2019-09-08 18:23:55 -0300

    UserInfoBox tests

--- src/views/shared/UserInfoBox/UserInfoBox.test.js
+++ src/views/shared/UserInfoBox/UserInfoBox.test.js
@@ -7,7 +7,7 @@ describe('<UserInfoBox />', () => {
     beforeEach(() => {
         const props = {
             user: {
-                name: 'pepito gonzales',
+                name: { title: 'sr', first: 'pepito', last: 'gonzales' },
                 email: 'pepito@mail.com',
                 location: 'Santiago de Chile',
                 login: 'pepitoUser',
@@ -22,5 +22,12 @@ describe('<UserInfoBox />', () => {
     })
     test('Deberia renderear un contenedor div con la clase user', () => {
         expect(shallowComp.find('div.user')).toHaveLength(1)
+    })
+    test('Deberia renderear 6 <p>', () => {
+        expect(shallowComp.find('p')).toHaveLength(6)
+    })
+    test('Deberia renderar el nombre del usuario', () => {
+        const userName = shallowComp.find('p.name').text().split(' ').filter(item => item.includes('pepito') || item.includes('gonzales'))
+        expect(userName).toHaveLength(2)
     })
 })
